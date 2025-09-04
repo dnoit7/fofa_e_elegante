8531d769107584ebd68ddc0cb08a611eb6ff391b
dnoit7 <dnoit7@gmail.com>
2025-09-03 23:26:27 -0300

    Update page.tsx
    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 
-import Hero from "@/components/Hero";
-import Catalog from "@/components/Catalog";
-import Testimonials from "@/components/Testimonials";
-import About from "@/components/About";
-import StoreInfo from "@/components/StoreInfo";
+import Hero from "../components/Hero";
+import Catalog from "../components/Catalog";
+import Testimonials from "../components/Testimonials";
+import About from "../components/About";
+import StoreInfo from "../components/StoreInfo";
 
 export default function Page() {
   return (
